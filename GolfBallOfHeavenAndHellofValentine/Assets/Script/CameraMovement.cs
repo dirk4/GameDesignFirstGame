0bff0a77358b4c482dd376496b6cbff687790cca
ASD <ngocson.bui0312@gmail.com>
2020-02-14 15:04:49 +0100

    Added cameramovement script

---
+++ GolfBallOfHeavenAndHellofValentine/Assets/Script/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position - ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ball.transform.position + position;
+    }
+}
