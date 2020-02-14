c864c4592c3e59f24d4cf9c0a3e755e90025f49b
jerko12 <61011416+jerko12@users.noreply.github.com>
2020-02-14 15:20:24 +0100

    Added Flyzone Prefab

---
+++ GolfBallOfHeavenAndHellofValentine/Assets/Script/HoverTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverTrigger : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>())
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            rb.AddForce(new Vector3(0,15,0));
+        }
+    }
+}
